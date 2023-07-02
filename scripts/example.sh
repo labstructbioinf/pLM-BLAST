768086a9b581c38cd4029d2115033325f0f7c4f1
sdh <s.dunin-horkawicz@cent.uw.edu.pl>
2023-07-02 15:21:31 +0200

    example

--- scripts/example.sh
+++ scripts/example.sh
@@ -6,16 +6,6 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-# Databases consist of two files, an index file (.csv) containing sequences and their descriptions, and an embedding file (.pt_emb.p)
-# containing PT5 (or other model) embeddings of sequences listed in the index.
-
-# To create a database, first use makeindex.py to create an index from a FASTA file,
-# and then use `embeddings.py` to compute embeddings based on this index file.
-
-# The query sequence must be represented as a one-element database.
-
-# See the example below for the complete pipeline from the FASTA sequence to searching the ECOD database.
-
 case='cupredoxin'
 
 # data paths
@@ -24,6 +14,7 @@ OUTDIR="./output"
 
 QUERY_INDEX="$OUTDIR/${case}.csv"
 OUTFILE="$OUTDIR/${case}.hits.csv"
+
 # Replace with a path to the database
 DB_PATH="/home/nfs/kkaminski/PLMBLST/ecod70db_20220902"
 
@@ -35,18 +26,17 @@ NUM_WORKERS=10
 
 mkdir -p $OUTDIR
 
-# calculate index
+# Calculate query index
 python makeindex.py ./input/$case.fas ./output/$case.csv
 
-# calculate embeddings
+# Calculate query embedding
 if [ ! -f $OUTDIR/$case.pt_emb.p ]; then
 	echo "calculate query embedding"
-	python ../embeddings.py $INDIR/$case.fas $OUTDIR/$case.pt_emb.p
+	python ../embeddings.py $INDIR/$case.fas $OUTDIR/$case.pt
 fi
 
-# run plm-blast
+# Run plm-blast
 if [ ! -f $OUTFILE ]; then
-	# search pre-calculated ECOD database
 	python run_plm_blast.py \
 		$DB_PATH \
 		$OUTDIR/$case \
