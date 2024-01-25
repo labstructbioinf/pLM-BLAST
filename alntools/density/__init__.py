4098fe5dadacfccce1fe72bac6db5a5258e80b62
Kamil Pawlicki <kpawlicki@edi07.cent1.uw.edu.pl>
2024-01-25 15:46:55 +0100

    Added pytest test_utils

--- alntools/density/__init__.py
+++ alntools/density/__init__.py
@@ -1,3 +1,3 @@
-from .local import get_multires_density, chunk_cosine_similarity, batch_slice_iterator
+from .local import get_multires_density, chunk_cosine_similarity, batch_slice_iterator, reduce_duplicates_query_filedict
 from .parallel import load_and_score_database
 from .parallel import load_full_embeddings
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
 import pytest
-
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from alntools.density.local import reduce_duplicates_query_filedict
 
 
 @pytest.mark.parametrize("size", [10, 50, 100])
@@ -8,12 +10,13 @@ def test_remove_duplicates(size):
     '''
     test if function properly filter redundant hits eg. a-b, a-b, a-c func should exclude b-a
     '''
-
-    data = {idx: list(range(size)) for idx in range(size)}
+    
+    filedict = {k: v for k, v in zip(range(size), range(size))}
+    data = {idx: filedict.copy() for idx in range(size)}
     # func
-
-    # expected number of samples is size*(size - 1)/2
-    expected_size = size*(size - 1)/2
+    reduce_duplicates_query_filedict(data)
+    # expected number of samples is size*(size - 1)/2 + size
+    expected_size = size*(size - 1)/2 + size
     actual_size = sum([len(v) for v in data.values()])
 
     assert actual_size == expected_size
